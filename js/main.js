c0e06f0f7519d1fb22a43e3cd94d541db5ea87da
AyslanOliver <ayslanoo37@hotmail.com>
2024-03-30 11:00:02 -0300

    #

--- js/main.js
+++ js/main.js
@@ -6,7 +6,24 @@ const loginForm = document.querySelector(".login-form");
 const registerForm = document.querySelector(".register-form");
 
 // Login button function
+loginBtn.addEventListener('click', () => {
+    loginBtn.style.backgrounColor ="#21264D";
+    registerBtn.style.backgrounColor = "rgba(255, 255, 255, 0.2)";
 
+    loginForm.style.left = "50%";
+    registerBtn.style.left = "-50%";
 
-
+    loginForm.style.opcity = 1;
+    registerForm.opacity = 0;
+})
 // Register button function
+registerBtn.addEventListener('click', () => {
+    loginBtn.style.backgrounColor ="rgba(255, 255, 255, 0.2)";
+    registerBtn.style.backgrounColor = "#21264D";
+
+    loginForm.style.left = "150%";
+    registerBtn.style.left = "50%";
+
+    loginForm.style.opcity = 0;
+    registerForm.opacity = 1;
+})
