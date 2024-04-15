71502288b0d8a95b3b90067cc0bd9a565a63aa77
AyslanOliver <ayslanoo37@hotmail.com>
2024-04-15 00:45:54 -0300

    #

--- php/view_empresa.php
+++ php/view_empresa.php
@@ -1,62 +1,81 @@
 <?php
-// Incluir o arquivo de conexão
-require 'conexao.php';
+// Dados de conexão com o banco de dados
+$host = 'localhost'; // endereço do servidor MySQL
+$usuario = 'Ayslan'; // nome de usuário do MySQL
+$senha = 'xlJ.44UFez3f2aMK'; // senha do MySQL
+$banco_de_dados = 'demolicao'; // nome do banco de dados a ser utilizado
 
-// Verificar se a conexão foi estabelecida com sucesso
-if ($conn === false) {
+
+// Criar conexão
+$conn = new mysqli($host, $usuario, $senha, $banco_de_dados);
+
+// Verificar conexão
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
+
+// Verificar se há erros de conexão
+if (!$conn) {
     die("Erro de conexão: " . mysqli_connect_error());
 }
 
 // Query para selecionar todos os dados da tabela cad_empresa
 $sql = "SELECT cnpj, razao_social, nome_fantasia, data_abertura, ddd_telefone_1, email, cep, logradouro, numero, bairro, municipio, uf FROM cad_empresa";
 
-// Executar consulta
-$result = $conn->query($sql);
+// Verificar se a conexão está estabelecida antes de executar a consulta
+if ($conn) {
+    // Executar a consulta
+    $result = $conn->query($sql);
 
-// Verificar se a consulta foi bem-sucedida
-if ($result) {
-    // Exibir cabeçalho da tabela
-    echo "<table class='table table-bordered' id='dataTable' width='100%' cellspacing='0'>
-            <thead>
-                <tr>
-                    <th>CNPJ</th>
-                    <th>Razão Social</th>
-                    <th>Nome Fantasia</th>
-                    <th>Data de Abertura</th>
-                    <th>Número de Contato</th>
-                    <th>E-mail</th>
-                    <th>CEP</th>
-                    <th>Logradouro</th>
-                    <th>Número</th>
-                    <th>Bairro</th>
-                    <th>Município</th>
-                    <th>Estado</th>
-                </tr>
-            </thead>
-            <tbody>";
+    // Verificar se a consulta foi bem-sucedida
+    if ($result) {
+        // Exibir cabeçalho da tabela
+        echo "<table class='table table-bordered' id='dataTable' width='100%' cellspacing='0'>
+                <thead>
+                    <tr>
+                        <th>CNPJ</th>
+                        <th>Razão Social</th>
+                        <th>Nome Fantasia</th>
+                        <th>Data de Abertura</th>
+                        <th>Número de Contato</th>
+                        <th>E-mail</th>
+                        <th>CEP</th>
+                        <th>Logradouro</th>
+                        <th>Número</th>
+                        <th>Bairro</th>
+                        <th>Município</th>
+                        <th>Estado</th>
+                    </tr>
+                </thead>
+                <tbody>";
 
-    // Exibir os dados na tabela
-    while($row = $result->fetch_assoc()) {
-        echo "<tr>
-                <td>".$row["cnpj"]."</td>
-                <td>".$row["razao_social"]."</td>
-                <td>".$row["nome_fantasia"]."</td>
-                <td>".$row["data_abertura"]."</td>
-                <td>".$row["ddd_telefone_1"]."</td>
-                <td>".$row["email"]."</td>
-                <td>".$row["cep"]."</td>
-                <td>".$row["logradouro"]."</td>
-                <td>".$row["numero"]."</td>
-                <td>".$row["bairro"]."</td>
-                <td>".$row["municipio"]."</td>
-                <td>".$row["uf"]."</td>
-              </tr>";
+        // Exibir os dados na tabela
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>
+                    <td>" . $row["cnpj"] . "</td>
+                    <td>" . $row["razao_social"] . "</td>
+                    <td>" . $row["nome_fantasia"] . "</td>
+                    <td>" . $row["data_abertura"] . "</td>
+                    <td>" . $row["ddd_telefone_1"] . "</td>
+                    <td>" . $row["email"] . "</td>
+                    <td>" . $row["cep"] . "</td>
+                    <td>" . $row["logradouro"] . "</td>
+                    <td>" . $row["numero"] . "</td>
+                    <td>" . $row["bairro"] . "</td>
+                    <td>" . $row["municipio"] . "</td>
+                    <td>" . $row["uf"] . "</td>
+                  </tr>";
+        }
+        echo "</tbody></table>";
+    } else {
+        echo "Erro ao executar consulta: " . $conn->error;
     }
-    echo "</tbody></table>";
 } else {
-    echo "Erro ao executar consulta: " . $conn->error;
+    echo "Não foi possível conectar ao banco de dados.";
 }
 
 // Fechar conexão
-$conn->close();
+if ($conn) {
+    $conn->close();
+}
 ?>
---
+++ sql/127_0_0_1.sql
@@ -0,0 +1,586 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 15/04/2024 às 05:06
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `demolicao`
+--
+CREATE DATABASE IF NOT EXISTS `demolicao` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `demolicao`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cad_cliente`
+--
+
+CREATE TABLE `cad_cliente` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(29) NOT NULL,
+  `genero` enum('M','F') NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `rg` varchar(20) NOT NULL,
+  `celular` varchar(15) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `cep` varchar(9) NOT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `numero` varchar(10) NOT NULL,
+  `bairro` varchar(100) DEFAULT NULL,
+  `cidade` varchar(100) DEFAULT NULL,
+  `estado` varchar(100) DEFAULT NULL,
+  `empresa_id` int(11) DEFAULT NULL,
+  `nascimento` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Despejando dados para a tabela `cad_cliente`
+--
+
+INSERT INTO `cad_cliente` (`id`, `nome`, `genero`, `cpf`, `rg`, `celular`, `email`, `cep`, `endereco`, `numero`, `bairro`, `cidade`, `estado`, `empresa_id`, `nascimento`) VALUES
+(2, 'Cauan Rossi de Sousa Lisboa', 'M', '042.432.055-07', '1116641798', '(71) 98887-7719', 'ayslano37@gmail.com', '41600-810', 'Rua Agnaldo Azevedo', '3', 'Stella Maris', 'Salvador', 'BA', NULL, '1995-03-06'),
+(3, 'Cristiane Pereira dos Santos ', 'F', '011.109.435-63', '1116641798', '(11) 11111-1111', 'sssd@gmail.com', '41730-145', 'Conjunto Vila dos Flamboyants', 's/n', 'Paralela', 'Salvador', 'BA', NULL, '1950-03-03');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cad_empresa`
+--
+
+CREATE TABLE `cad_empresa` (
+  `id` int(11) NOT NULL,
+  `cnpj` varchar(18) NOT NULL,
+  `razao_social` varchar(255) NOT NULL,
+  `nome_fantasia` varchar(255) DEFAULT NULL,
+  `data_abertura` date DEFAULT NULL,
+  `ddd_telefone_1` varchar(3) DEFAULT NULL,
+  `telefone` varchar(15) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `cep` varchar(9) DEFAULT NULL,
+  `logradouro` varchar(255) NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `municipio` varchar(100) NOT NULL,
+  `uf` varchar(2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Despejando dados para a tabela `cad_empresa`
+--
+
+INSERT INTO `cad_empresa` (`id`, `cnpj`, `razao_social`, `nome_fantasia`, `data_abertura`, `ddd_telefone_1`, `telefone`, `email`, `cep`, `logradouro`, `numero`, `bairro`, `municipio`, `uf`) VALUES
+(1, '48.703.009/0001-95', 'FIRE COMPANY LTDA', 'FIRE COMPANY', '0000-00-00', '(11', NULL, '', '04711-130', 'DR CHUCRI ZAIDAN', '1550', 'VILA SÃO FRANCISCO (ZONA SUL)', 'SAO PAULO', 'SP'),
+(2, '47.776.879/0001-21', 'EDIVALDO RIBEIRO NERY JUNIOR 01317103114', 'EDITUDO LTDA', '0000-00-00', '(61', NULL, '', '72622-104', 'QUADRA 308 CONJUNTO 4', '10', 'RECANTO DAS EMAS', 'BRASILIA', 'DF'),
+(3, '34.309.079/0001-59', 'G10 SISTEMAS E CONSULTORIA EM TECNOLOGIA DA INFORMACAO LTDA', 'G10 SISTEMAS', '0000-00-00', '(75', NULL, '', '44434-532', 'TEODORO DIAS BARRETO', '288', 'ANDAIA', 'SANTO ANTONIO DE JESUS', 'BA'),
+(4, '38.129.322/0001-08', 'ANA INEZ EURICO DOS SANTOS PEREIRA 35415650806', 'INEZ EURICO', '0000-00-00', '(11', NULL, '', '04476-217', 'LUCIA PARENTE SERRA', '132', 'JARDIM DAS LARANJEIRAS (ZONA SUL)', 'SAO PAULO', 'SP'),
+(5, '44.068.169/0001-59', 'SCM FIRE TECNOLOGIA DA INFORMACAO LTDA', 'SCM FIRE', '0000-00-00', '(47', NULL, '', '89211-060', 'DIAMANTINA', '281', 'FLORESTA', 'JOINVILLE', 'SC');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cad_cliente`
+--
+ALTER TABLE `cad_cliente`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `empresa_id` (`empresa_id`);
+
+--
+-- Índices de tabela `cad_empresa`
+--
+ALTER TABLE `cad_empresa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cad_cliente`
+--
+ALTER TABLE `cad_cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `cad_empresa`
+--
+ALTER TABLE `cad_empresa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- Banco de dados: `phpmyadmin`
+--
+CREATE DATABASE IF NOT EXISTS `phpmyadmin` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+USE `phpmyadmin`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__bookmark`
+--
+
+CREATE TABLE `pma__bookmark` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `dbase` varchar(255) NOT NULL DEFAULT '',
+  `user` varchar(255) NOT NULL DEFAULT '',
+  `label` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `query` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Bookmarks';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__central_columns`
+--
+
+CREATE TABLE `pma__central_columns` (
+  `db_name` varchar(64) NOT NULL,
+  `col_name` varchar(64) NOT NULL,
+  `col_type` varchar(64) NOT NULL,
+  `col_length` text DEFAULT NULL,
+  `col_collation` varchar(64) NOT NULL,
+  `col_isNull` tinyint(1) NOT NULL,
+  `col_extra` varchar(255) DEFAULT '',
+  `col_default` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Central list of columns';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__column_info`
+--
+
+CREATE TABLE `pma__column_info` (
+  `id` int(5) UNSIGNED NOT NULL,
+  `db_name` varchar(64) NOT NULL DEFAULT '',
+  `table_name` varchar(64) NOT NULL DEFAULT '',
+  `column_name` varchar(64) NOT NULL DEFAULT '',
+  `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `mimetype` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `transformation` varchar(255) NOT NULL DEFAULT '',
+  `transformation_options` varchar(255) NOT NULL DEFAULT '',
+  `input_transformation` varchar(255) NOT NULL DEFAULT '',
+  `input_transformation_options` varchar(255) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Column information for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__designer_settings`
+--
+
+CREATE TABLE `pma__designer_settings` (
+  `username` varchar(64) NOT NULL,
+  `settings_data` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Settings related to Designer';
+
+--
+-- Despejando dados para a tabela `pma__designer_settings`
+--
+
+INSERT INTO `pma__designer_settings` (`username`, `settings_data`) VALUES
+('root', '{\"angular_direct\":\"direct\",\"snap_to_grid\":\"off\",\"relation_lines\":\"true\"}');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__export_templates`
+--
+
+CREATE TABLE `pma__export_templates` (
+  `id` int(5) UNSIGNED NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `export_type` varchar(10) NOT NULL,
+  `template_name` varchar(64) NOT NULL,
+  `template_data` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Saved export templates';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__favorite`
+--
+
+CREATE TABLE `pma__favorite` (
+  `username` varchar(64) NOT NULL,
+  `tables` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Favorite tables';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__history`
+--
+
+CREATE TABLE `pma__history` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `db` varchar(64) NOT NULL DEFAULT '',
+  `table` varchar(64) NOT NULL DEFAULT '',
+  `timevalue` timestamp NOT NULL DEFAULT current_timestamp(),
+  `sqlquery` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='SQL history for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__navigationhiding`
+--
+
+CREATE TABLE `pma__navigationhiding` (
+  `username` varchar(64) NOT NULL,
+  `item_name` varchar(64) NOT NULL,
+  `item_type` varchar(64) NOT NULL,
+  `db_name` varchar(64) NOT NULL,
+  `table_name` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Hidden items of navigation tree';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__pdf_pages`
+--
+
+CREATE TABLE `pma__pdf_pages` (
+  `db_name` varchar(64) NOT NULL DEFAULT '',
+  `page_nr` int(10) UNSIGNED NOT NULL,
+  `page_descr` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='PDF relation pages for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__recent`
+--
+
+CREATE TABLE `pma__recent` (
+  `username` varchar(64) NOT NULL,
+  `tables` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Recently accessed tables';
+
+--
+-- Despejando dados para a tabela `pma__recent`
+--
+
+INSERT INTO `pma__recent` (`username`, `tables`) VALUES
+('root', '[{\"db\":\"demolicao\",\"table\":\"cad_empresa\"},{\"db\":\"demolicao\",\"table\":\"cad_cliente\"},{\"db\":\"site\",\"table\":\"cad_cliente\"}]');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__relation`
+--
+
+CREATE TABLE `pma__relation` (
+  `master_db` varchar(64) NOT NULL DEFAULT '',
+  `master_table` varchar(64) NOT NULL DEFAULT '',
+  `master_field` varchar(64) NOT NULL DEFAULT '',
+  `foreign_db` varchar(64) NOT NULL DEFAULT '',
+  `foreign_table` varchar(64) NOT NULL DEFAULT '',
+  `foreign_field` varchar(64) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Relation table';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__savedsearches`
+--
+
+CREATE TABLE `pma__savedsearches` (
+  `id` int(5) UNSIGNED NOT NULL,
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `db_name` varchar(64) NOT NULL DEFAULT '',
+  `search_name` varchar(64) NOT NULL DEFAULT '',
+  `search_data` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Saved searches';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__table_coords`
+--
+
+CREATE TABLE `pma__table_coords` (
+  `db_name` varchar(64) NOT NULL DEFAULT '',
+  `table_name` varchar(64) NOT NULL DEFAULT '',
+  `pdf_page_number` int(11) NOT NULL DEFAULT 0,
+  `x` float UNSIGNED NOT NULL DEFAULT 0,
+  `y` float UNSIGNED NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Table coordinates for phpMyAdmin PDF output';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__table_info`
+--
+
+CREATE TABLE `pma__table_info` (
+  `db_name` varchar(64) NOT NULL DEFAULT '',
+  `table_name` varchar(64) NOT NULL DEFAULT '',
+  `display_field` varchar(64) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Table information for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__table_uiprefs`
+--
+
+CREATE TABLE `pma__table_uiprefs` (
+  `username` varchar(64) NOT NULL,
+  `db_name` varchar(64) NOT NULL,
+  `table_name` varchar(64) NOT NULL,
+  `prefs` text NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Tables'' UI preferences';
+
+--
+-- Despejando dados para a tabela `pma__table_uiprefs`
+--
+
+INSERT INTO `pma__table_uiprefs` (`username`, `db_name`, `table_name`, `prefs`, `last_update`) VALUES
+('root', 'demolicao', 'cad_empresa', '{\"sorted_col\":\"`cad_empresa`.`id` ASC\"}', '2024-04-15 02:21:08');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__tracking`
+--
+
+CREATE TABLE `pma__tracking` (
+  `db_name` varchar(64) NOT NULL,
+  `table_name` varchar(64) NOT NULL,
+  `version` int(10) UNSIGNED NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime NOT NULL,
+  `schema_snapshot` text NOT NULL,
+  `schema_sql` text DEFAULT NULL,
+  `data_sql` longtext DEFAULT NULL,
+  `tracking` set('UPDATE','REPLACE','INSERT','DELETE','TRUNCATE','CREATE DATABASE','ALTER DATABASE','DROP DATABASE','CREATE TABLE','ALTER TABLE','RENAME TABLE','DROP TABLE','CREATE INDEX','DROP INDEX','CREATE VIEW','ALTER VIEW','DROP VIEW') DEFAULT NULL,
+  `tracking_active` int(1) UNSIGNED NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Database changes tracking for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__userconfig`
+--
+
+CREATE TABLE `pma__userconfig` (
+  `username` varchar(64) NOT NULL,
+  `timevalue` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `config_data` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='User preferences storage for phpMyAdmin';
+
+--
+-- Despejando dados para a tabela `pma__userconfig`
+--
+
+INSERT INTO `pma__userconfig` (`username`, `timevalue`, `config_data`) VALUES
+('root', '2024-04-15 03:05:57', '{\"Console\\/Mode\":\"show\",\"lang\":\"pt_BR\",\"ThemeDefault\":\"pmahomme\",\"NavigationWidth\":0}');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__usergroups`
+--
+
+CREATE TABLE `pma__usergroups` (
+  `usergroup` varchar(64) NOT NULL,
+  `tab` varchar(64) NOT NULL,
+  `allowed` enum('Y','N') NOT NULL DEFAULT 'N'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='User groups with configured menu items';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pma__users`
+--
+
+CREATE TABLE `pma__users` (
+  `username` varchar(64) NOT NULL,
+  `usergroup` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Users and their assignments to user groups';
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `pma__bookmark`
+--
+ALTER TABLE `pma__bookmark`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `pma__central_columns`
+--
+ALTER TABLE `pma__central_columns`
+  ADD PRIMARY KEY (`db_name`,`col_name`);
+
+--
+-- Índices de tabela `pma__column_info`
+--
+ALTER TABLE `pma__column_info`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `db_name` (`db_name`,`table_name`,`column_name`);
+
+--
+-- Índices de tabela `pma__designer_settings`
+--
+ALTER TABLE `pma__designer_settings`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Índices de tabela `pma__export_templates`
+--
+ALTER TABLE `pma__export_templates`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `u_user_type_template` (`username`,`export_type`,`template_name`);
+
+--
+-- Índices de tabela `pma__favorite`
+--
+ALTER TABLE `pma__favorite`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Índices de tabela `pma__history`
+--
+ALTER TABLE `pma__history`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`,`db`,`table`,`timevalue`);
+
+--
+-- Índices de tabela `pma__navigationhiding`
+--
+ALTER TABLE `pma__navigationhiding`
+  ADD PRIMARY KEY (`username`,`item_name`,`item_type`,`db_name`,`table_name`);
+
+--
+-- Índices de tabela `pma__pdf_pages`
+--
+ALTER TABLE `pma__pdf_pages`
+  ADD PRIMARY KEY (`page_nr`),
+  ADD KEY `db_name` (`db_name`);
+
+--
+-- Índices de tabela `pma__recent`
+--
+ALTER TABLE `pma__recent`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Índices de tabela `pma__relation`
+--
+ALTER TABLE `pma__relation`
+  ADD PRIMARY KEY (`master_db`,`master_table`,`master_field`),
+  ADD KEY `foreign_field` (`foreign_db`,`foreign_table`);
+
+--
+-- Índices de tabela `pma__savedsearches`
+--
+ALTER TABLE `pma__savedsearches`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `u_savedsearches_username_dbname` (`username`,`db_name`,`search_name`);
+
+--
+-- Índices de tabela `pma__table_coords`
+--
+ALTER TABLE `pma__table_coords`
+  ADD PRIMARY KEY (`db_name`,`table_name`,`pdf_page_number`);
+
+--
+-- Índices de tabela `pma__table_info`
+--
+ALTER TABLE `pma__table_info`
+  ADD PRIMARY KEY (`db_name`,`table_name`);
+
+--
+-- Índices de tabela `pma__table_uiprefs`
+--
+ALTER TABLE `pma__table_uiprefs`
+  ADD PRIMARY KEY (`username`,`db_name`,`table_name`);
+
+--
+-- Índices de tabela `pma__tracking`
+--
+ALTER TABLE `pma__tracking`
+  ADD PRIMARY KEY (`db_name`,`table_name`,`version`);
+
+--
+-- Índices de tabela `pma__userconfig`
+--
+ALTER TABLE `pma__userconfig`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Índices de tabela `pma__usergroups`
+--
+ALTER TABLE `pma__usergroups`
+  ADD PRIMARY KEY (`usergroup`,`tab`,`allowed`);
+
+--
+-- Índices de tabela `pma__users`
+--
+ALTER TABLE `pma__users`
+  ADD PRIMARY KEY (`username`,`usergroup`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `pma__bookmark`
+--
+ALTER TABLE `pma__bookmark`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pma__column_info`
+--
+ALTER TABLE `pma__column_info`
+  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pma__export_templates`
+--
+ALTER TABLE `pma__export_templates`
+  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pma__history`
+--
+ALTER TABLE `pma__history`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pma__pdf_pages`
+--
+ALTER TABLE `pma__pdf_pages`
+  MODIFY `page_nr` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pma__savedsearches`
+--
+ALTER TABLE `pma__savedsearches`
+  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
